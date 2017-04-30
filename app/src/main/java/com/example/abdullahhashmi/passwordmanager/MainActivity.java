691200f57bdb7c59b6a02b8433103c96e1f50013
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2017-04-30 19:42:08 -0400

    Adding some more changes

--- app/src/main/java/com/example/abdullahhashmi/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/abdullahhashmi/passwordmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.abdullahhashmi.passwordmanager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = this;
         TextView login1 = (TextView)findViewById(R.id.textView);
         final EditText password = (EditText)findViewById(R.id.editText);
 
@@ -131,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-
+                    Toast.makeText(context, "Wrong login! Try again." , Toast.LENGTH_SHORT).show();
                 }
 
             }
