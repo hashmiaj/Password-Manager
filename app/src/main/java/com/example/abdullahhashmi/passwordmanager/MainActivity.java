36df3c9e70eed283378acd0b4c51eab68bb3e2b6
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2017-04-30 18:48:11 -0400

    …

--- app/src/main/java/com/example/abdullahhashmi/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/abdullahhashmi/passwordmanager/MainActivity.java
@@ -5,24 +5,138 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView login1 = (TextView)findViewById(R.id.textView);
+        final EditText password = (EditText)findViewById(R.id.editText);
 
-        Button submitButton = (Button)findViewById(R.id.loginBtn);
+        Button key1 =(Button)findViewById(R.id.key1) ;
+        key1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("1");
+            }
+        } );
 
-        submitButton.setOnClickListener(new View.OnClickListener()
-        {
+        Button key2 =(Button)findViewById(R.id.key2) ;
+        key2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("2");
+            }
+        } );
+
+        Button key3 =(Button)findViewById(R.id.key3) ;
+        key3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("3");
+            }
+        } );
+
+        Button key4 =(Button)findViewById(R.id.key4) ;
+        key4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("4");
+            }
+        } );
+
+        Button key5 =(Button)findViewById(R.id.key5) ;
+        key5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("5");
+            }
+        } );
+
+        Button key6 =(Button)findViewById(R.id.key6) ;
+        key6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("6");
+            }
+        } );
+
+        Button key7 =(Button)findViewById(R.id.key7) ;
+        key7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("7");
+            }
+        } );
+
+        Button key8 =(Button)findViewById(R.id.key8) ;
+        key8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("8");
+            }
+        } );
+
+        Button key9 =(Button)findViewById(R.id.key9) ;
+        key9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("9");
+            }
+        } );
+
+        Button key0 =(Button)findViewById(R.id.key0) ;
+        key0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.append("0");
+            }
+        } );
+
+        Button clear =(Button)findViewById(R.id.keyClear) ;
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.setText(null);
+            }
+        } );
+
+        Button backSpace =(Button)findViewById(R.id.backSpace) ;
+        backSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passwordStr = password.getText().toString();
+                if (passwordStr.length() > 0) {
+                    String newPasswordStr = new StringBuilder(passwordStr)
+                            .deleteCharAt(passwordStr.length() - 1).toString();
+                    password.setText(newPasswordStr);
+                }
+
+            }
+        } );
+
+        Button submit =(Button)findViewById(R.id.submit) ;
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
             public void onClick(View v)
             {
-                startActivity(new Intent(MainActivity.this, ServicesActivity.class));
+                if(password.getText().toString().equals("0000")) {
+                    Intent intent = new Intent(MainActivity.this, ServicesActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+
+                }
+
             }
-        });
+        } );
 
     }
 }
+
